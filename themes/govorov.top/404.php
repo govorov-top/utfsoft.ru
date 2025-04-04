6300769803bd08ce848a19160dbc3a85cfe429da
Stanislaw Timofeew <stastimofeew98@gmail.com>
2025-04-04 11:36:14 +0300

    added 404 page

--- themes/govorov.top/404.php
+++ themes/govorov.top/404.php
@@ -8,24 +8,33 @@
  */
 
 get_header(); ?>
-<main id="main" class="site-main">
-    <section class="error-404 not-found">
+<main id="main" class="site-main page-404">
+    <section class="error-404 pt-0 not-found">
         <div class="container">
-            <h1 class="pb-0">Упс! Что-то пошло не так...</h1>
-            <p style="font-size: 70px; text-align: center; margin: 100px 0">Ошибка 404</p>
-            <div class="items btn_center d-flex flex-wrap justify-content-start align-items-center pb-4"
-                 style="max-width: 425px">
-                <div class="item me-3 mb-sm-3 mb-xl-0 mb-2">
-                    <p class="mb-0"><a href="/" class="btn">Главная страница</a></p>
+            <div class="content rg-bg-box position-relative d-flex flex-column">
+                <div class="wrapper d-flex flex-column flex-grow-1 mb-4 mb-sm-0">
+                    <h1 class="pb-4">
+                        Страница, которую вы пытаетесь <br class="d-none d-md-block"> просмотреть, не существует...
+                    </h1>
+                    <button class="btn link-js btn_max-content mt-auto " data-link="/">На главную</button>
                 </div>
-                <div class="item">
-                    <p class="mb-0"><a href="/o-sisteme/" class="btn btn_link btn_icon bi bi-arrow-right">О сервисе</a>
-                    </p>
-                </div>
+
+                <picture class="image">
+                    <source media="(max-width:576.98px)"
+                            srcset="/wp-content/themes/govorov.top/assets/img/404/404-576.svg">
+                    <source media="(max-width:767.98px)"
+                            srcset="/wp-content/themes/govorov.top/assets/img/404/404-768.svg">
+                    <source media="(max-width:991.98px)"
+                            srcset="/wp-content/themes/govorov.top/assets/img/404/404-992.svg">
+                    <source media="(max-width:1199.98px)"
+                            srcset="/wp-content/themes/govorov.top/assets/img/404/404-1200.svg">
+                    <source media="(max-width:1399.98px)"
+                            srcset="/wp-content/themes/govorov.top/assets/img/404/404-1400.svg">
+                    <img src="/wp-content/themes/govorov.top/assets/img/404/404.svg" alt="404">
+                </picture>
             </div>
         </div>
     </section><!-- .error-404 -->
-	<?= do_shortcode( '[rg-code f="repeat-elements/fast-connect-form.php"]' ) ?>
 </main><!-- .site-main -->
 
 <?php get_footer(); ?>
